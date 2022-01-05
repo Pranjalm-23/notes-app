040a8173ad410be2617c53089799ef7a52f67d78
Pranjal Mishra <pranjal.ug19.cs@nitp.ac.in>
2022-01-05 21:42:04 +0530

    list feature added

--- app.js
+++ app.js
@@ -50,7 +50,8 @@ yargs.command({
   command: "list",
   describe: "List your notes",
   handler() {
-    console.log("Listing out all notes");
+    //console.log("Listing out all notes");
+    notes.listNotes();
   },
 });
 
